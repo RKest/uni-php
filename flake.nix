5767739a14cde0df64826723ff444e136ea199a1
RKest <max.ind@o2.pl>
2024-10-25 03:47:15 +0200

    Some test commit

--- flake.nix
+++ flake.nix
@@ -6,14 +6,79 @@
   };
 
   outputs = { self, nixpkgs }:
-  let 
+  let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
+    mysql_datadir = "./mysql_data";
+    mysql_socket = "/tmp/mysql.sock";
+    mysql_pid_file = "/tmp/mysql.pid";
+
+    init_script = pkgs.writeScriptBin "init_mysql" ''
+      #!${pkgs.stdenv.shell}
+
+      if [ ! -d "${mysql_datadir}" ]; then
+        echo "Initializing MySQL data directory..."
+        mkdir -p ${mysql_datadir}
+        ${pkgs.mysql84}/bin/mysqld --initialize-insecure \
+          --datadir=${mysql_datadir} \
+          --user=$(whoami)
+      fi
+    '';
+
+    start_script = pkgs.writeScriptBin "start_mysql" ''
+      #!${pkgs.stdenv.shell}
+
+      if [ ! -S "${mysql_socket}" ]; then
+        echo "Starting MySQL server..."
+        ${pkgs.mysql84}/bin/mysqld \
+          --datadir=${mysql_datadir} \
+          --socket=${mysql_socket} \
+          --pid-file=${mysql_pid_file} \
+          --user=$(whoami) &
+
+        # Wait for MySQL to start
+        until [ -s "${mysql_socket}" ]; do
+          sleep 1
+        done
+        echo "MySQL server started"
+      else
+        echo "MySQL server is already running"
+      fi
+    '';
+
+    stop_script = pkgs.writeScriptBin "stop_mysql" ''
+      #!${pkgs.stdenv.shell}
+
+      if [ -f "${mysql_pid_file}" ]; then
+        echo "Stopping MySQL server"
+        kill $(cat ${mysql_pid_file})
+        rm -f ${mysql_pid_file}
+        rm -f ${mysql_socket}
+        echo "MySQL server stopped"
+      else
+        echo "MySQL server not running"
+      fi
+    '';
+
+    set_password = pkgs.writeScriptBin "set_mysql_passwd" ''
+      #!${pkgs.stdenv.shell}
+
+      set -eu
+      
+      mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY $MYSQL_PASSWD"
+    '';
   in
   {
-    devShells.${system}.default = pkgs.stdenv.mkDerivation {
+    devShells.${system}.default = pkgs.mkShell {
       name = "multi-php";
-      nativeBuildInputs = [pkgs.php pkgs.phpactor];
+      nativeBuildInputs = [pkgs.phpactor];
+      buildInputs = [pkgs.php pkgs.mysql84 init_script start_script stop_script set_password];
+
+      shellHook = ''
+        export MYSQL_UNIX_PORT=${mysql_socket}
+        init_mysql && start_mysql && echo "MySQL ready, connect with: mysql -u root" || echo "Error starting mysql server"
+      '';
     };
   };
 }
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
+<?php declare(strict_types=1);
 
-session_start();
+$user = htmlentities($_POST['user'], ENT_QUOTES, 'UTF-8');
+$pass = htmlentities($_POST['pass'], ENT_QUOTES, 'UTF-8');
+$link = mysqli_connect('localhost', 'db_user', 'db_password', 'db_name');
+
+if (false == $link) {
+    echo "Error: " . mysqli_connect_error();
+    exit();
+}
+
+$result = mysqli_query($link, "SELECT * FROM users WHERE username='$user'");
+$record = mysqli_fetch_array($result);
+
+if ($record && $record['password'] === $pass) {
+    echo "Login successful";
+} else {
+    echo "Invalid login or password";
+}
+mysqli_close($link);
+
+?>
