a710daa4907980068099f2d75f50c245609092be
RKest <max.ind@o2.pl>
2025-05-17 21:46:24 +0200

    Added user creation

--- init/z11.sql
+++ init/z11.sql
@@ -16,6 +16,11 @@ CREATE TABLE logins (
     FOREIGN KEY (uid) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+INSERT INTO `users` (`id`, `username`, `password`, `image`) VALUES
+(1, 'user1', 'pass1', null),
+(2, 'user2', 'pass2', null),
+(3, 'admin', 'admin', null);
+
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) COLLATE utf8_polish_ci NOT NULL,
--- init/z8.sql
+++ init/z8.sql
@@ -15,3 +15,8 @@ CREATE TABLE logins (
     state INT NOT NULL,
     FOREIGN KEY (uid) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+INSERT INTO `users` (`id`, `username`, `password`, `image`) VALUES
+(1, 'user1', 'pass1', null),
+(2, 'user2', 'pass2', null),
+(3, 'admin', 'admin', null);
